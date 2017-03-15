1fb19424cb4c1736a8d5577dac655acad42fc013
Igor Konovalov <konovalov.avp@gmail.com>
2017-03-15 17:38:06 +0300

    laset pre production test

--- src/shared/config.js
+++ src/shared/config.js
@@ -2,7 +2,7 @@
 
 export const WEB_PORT = process.env.PORT || 8000
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'JS Stack from scratch!'
+export const APP_NAME = 'Tic Tac Toe Multiplayer game on React + Socket.io!'
 
 export const WDS_PORT = 7000
 export const APP_CONTAINER_CLASS = 'js-app'
